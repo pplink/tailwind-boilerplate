5b825b7deb0e3cf0421865980b94015a3d2f80cc
Jaeseong Seo <07js23@gmail.com>
2021-09-26 15:17:12 +0900

    Add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import Page1 from "./Page1";
+import Page2 from "./Page2";
+import Page3 from "./Page3";
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/page1">
+          <Page1 />
+        </Route>
+        <Route path="/page2">
+          <Page2 />
+        </Route>
+        <Route path="/page3">
+          <Page3 />
+        </Route>
+        <Route path="/">
+          <div>Hello World!</div>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
