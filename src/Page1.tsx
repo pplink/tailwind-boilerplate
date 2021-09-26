ea6876516575cc715bf16ffaa454ebc7ddfa8a34
verbalk <verbalk89@naver.com>
2021-09-26 19:34:35 +0900

    page1

--- src/Page1.tsx
+++ src/Page1.tsx
@@ -6,27 +6,34 @@ function Page1() {
       <div className="w-screen h-screen">
         <div className="flex flex-row">
           <div className="w-3/12 h-screen bg-gray-800">
-            <div>list</div>
-            <div>me</div>
-            <div>participants</div>
-            <div>bottom</div>
+            <div className="h-16 text-white p-4  shadow-lg">LIST</div>
+            <ul className="text-white">
+              <li className="p-4 border border-indigo-600 flex">꼼꼼한양</li>
+              <li className="p-4">꼼꼼한양</li>
+              <li className="p-4">꼼꼼한양</li>
+              <li className="p-4">꼼꼼한양</li>
+            </ul>
           </div>
           <div className="w-6/12 text-gray-400 space-x-1">canvas</div>
           <div className="relative w-3/12 h-screen bg-gray-800">
             <div className="h-16 text-white p-4  shadow-lg">CHAT</div>
             <div className="flex p-4 space-x-2">
-              <div className="rounded-full text-white bg-indigo-600">
-                <div className="rounded-full p-4">꼼양</div>
+              <div className="rounded-full text-white bg-indigo-600 w-14 h-14 ">
+                <p className="my-4"></p>
               </div>
               <div>
-                <p className="text-gray-200 text-xs">꼼꼼한양</p>
-                <p className="p-2 w-40 bg-gray-900 text-gray-300">안녕하세요</p>
+                <p className="text-gray-200 text-xs my-1">꼼꼼한양</p>
+                <p className="rounded-md p-2 w-40 bg-gray-900 text-gray-300">
+                  안녕하세요
+                </p>
               </div>
             </div>
-            <div className="absolute inset-x-0 bottom-0 flex text-white p-4 border border-indigo-600">
-              <div className="border rounded-md border-indigo-600 w-9/12 bg-gray-900">
-                <p className="p-2">text..</p>
-              </div>
+            <div className="absolute inset-x-0 bottom-0 flex text-white p-4 border-t border-gray-900">
+              <input
+                className="border rounded-md border-indigo-600 w-9/12 bg-gray-900 p-1"
+                type="text"
+                placeholder="text.."
+              />
               <div className="w-3/12 p-2 text-center">emoji</div>
             </div>
           </div>
