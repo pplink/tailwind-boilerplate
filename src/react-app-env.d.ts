344a054822380e16d5b24af7d0fc39ab516c8064
Jaeseong Seo <07js23@gmail.com>
2021-12-10 13:17:37 +0900

    Install boxicons

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,19 @@
 /// <reference types="react-scripts" />
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    "box-icon": BoxIconAttributes;
+  }
+
+  interface BoxIconAttributes extends React.HTMLProps<HTMLElement> {
+    name: string;
+    type?: "regular" | "solid" | "logo";
+    color?: string;
+    size?: "xs" | "sm" | "md" | "lg" | "cssSize";
+    rotate?: "90" | "180" | "270";
+    flip?: "horizontal" | "vertical";
+    border?: "square" | "circle";
+    animation?: string;
+    pull?: "left" | "right";
+  }
+}
