60d8010d99fb8b381064072330dbed782171863b
Jaeseong Seo <07js23@gmail.com>
2021-12-10 13:19:57 +0900

    Add a circle

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div className="fixed inset-0 bg-gray-100 flex">
+      <div className="rounded-full bg-white w-12 h-12 m-auto" />
+    </div>
+  );
 }
 
 export default App;
